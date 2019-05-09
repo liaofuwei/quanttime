e71769390ac2a7d21943766cae4e4414be9e6ca0
liaofuwei <459193023@qq.com>
2019-05-09 15:58:03 +0800

    增加券商估值分析监控

--- src/watch_time/watchMainWindows.py
+++ src/watch_time/watchMainWindows.py
@@ -9,6 +9,13 @@ import pandas as pd
 import numpy as np
 import tushare as ts
 import math
+import pysnooper
+
+'''
+功能：
+1、银行分红率排名，flitter排名
+2、券商估值分析
+'''
 
 class watchMainWindows(object):
     def __init__(self):
@@ -32,11 +39,17 @@ class watchMainWindows(object):
         self.ui.checkBox_5.setCheckState(QtCore.Qt.Checked)
         self.ui.checkBox_7.setCheckState(QtCore.Qt.Unchecked)
 
+        # 默认只分析重点关注的券商标的
+        self.ui.checkBox.setCheckState(QtCore.Qt.Checked)
+
         # 银行基本信息更新按钮信号连接
         self.ui.pushButton.clicked.connect(self.update_bank_industry_table)
 
         # 银行分红信息及系统排名表刷新信号连接
         self.ui.pushButton_2.clicked.connect(self.update_bank_dividend)
+
+        # 券商估值分析pushbutton按钮signal连接
+        self.ui.pushButton_3.clicked.connect(self.analyze_security_valuation)
 
         main_window.show()
         sys.exit(app.exec_())
@@ -155,6 +168,166 @@ class watchMainWindows(object):
 
     # ==============================================================
 
+    # @pysnooper.snoop()
+    def analyze_security_valuation(self):
+        '''
+        分析券商的估值信息
+        分别读取所有券商valuation表，获取pb，pe等信息
+        :return:
+        '''
+        security = pd.DataFrame()
+        check_status = self.ui.checkBox.checkState()
+        if check_status == QtCore.Qt.Checked:
+            # 读取重点关注的券商
+            security = pd.read_csv(r"C:\quanttime\src\watch_time\key_security.csv", encoding="gbk",
+                                   index_col=["ts_code"])
+        else:
+            # 从tushare的基本信息表中读取所有证券行业的code和名称
+            select_columns = ["ts_code", "name", "industry"]
+            security = pd.read_csv(r"C:\quanttime\data\basic_info\all_stock_info_ts.csv",
+                                   usecols=select_columns, encoding="gbk", index_col=["ts_code"])
+            security = security[security["industry"] == "证券"]
+
+        # loc获取元素时，name是属性，使用loc[index,['name']].name获取的值不对，所有列名这里改个名称
+        security = security.rename(columns={"name": "sname"})
+
+        # 读取所有证券行业的valuation数据，该数据来至于finance文件夹下的valuation表，该表每天更新，存到交易日前一天的股指数据
+        get_columns = ["code", "day", "pe_ratio", "pb_ratio"]
+        basic_path = "C:\\quanttime\\data\\finance\\valuation\\"
+        pb_general_result = []
+        pe_general_result = []
+        test_code_list = ['600030.SH', '000776.SZ']
+        #for ts_code in test_code_list:
+        for ts_code in security.index:
+            jq_code = self.tscode2jqcode(ts_code)
+            if jq_code == "000000":
+                continue
+            file_path = basic_path + jq_code + ".csv"
+            if os.path.exists(file_path):
+                df_valuation = pd.read_csv(file_path, usecols=get_columns)
+                df_valuation = df_valuation.drop_duplicates("day")
+                df_valuation = df_valuation.set_index("day")
+                if df_valuation.empty:
+                    continue
+
+                pb = df_valuation.loc[df_valuation.index[-1], ["pb_ratio"]].pb_ratio
+                pb_quantile_5 = df_valuation.pb_ratio.quantile(0.05)
+                pb_quantile_10 = df_valuation.pb_ratio.quantile(0.10)
+                pb_min = df_valuation.pb_ratio.min()
+                pb_min_date = df_valuation.pb_ratio.idxmin()
+                pb_max = df_valuation.pb_ratio.max()
+                pb_max_date = df_valuation.pb_ratio.idxmax()
+                pb_median = df_valuation.pb_ratio.median()
+                pb_std = df_valuation.pb_ratio.std()
+
+                pb_name = security.loc[ts_code, ["sname"]].sname
+                price = "--"
+                pre_close = "--"
+                df_quote = self.get_quote([ts_code])
+                if not df_quote.empty:
+                    df_quote = df_quote.set_index("code")
+                    price = df_quote.iloc[0, df_quote.columns.get_loc('price')]
+                    pre_close = df_quote.iloc[0, df_quote.columns.get_loc('pre_close')]
+                pb_tmp = [ts_code, pb_name, price, pre_close, pb, pb_min, pb_min_date, pb_quantile_5, pb_quantile_10,
+                          pb_median, pb_max, pb_max_date, pb_std]
+                pb_general_result.append(pb_tmp)
+
+                pe = df_valuation.loc[df_valuation.index[-1], ["pe_ratio"]].pe_ratio
+                pe_quantile_5 = df_valuation.pe_ratio.quantile(0.05)
+                pe_quantile_10 = df_valuation.pe_ratio.quantile(0.10)
+                pe_min = df_valuation.pe_ratio.min()
+                pe_min_date = df_valuation.pe_ratio.idxmin()
+                pe_max = df_valuation.pe_ratio.max()
+                pe_max_date = df_valuation.pe_ratio.idxmax()
+                pe_median = df_valuation.pe_ratio.median()
+                pe_std = df_valuation.pe_ratio.std()
+                pe_tmp = [ts_code, pb_name, price, pre_close, pe, pe_min, pe_min_date, pe_quantile_5, pe_quantile_10,
+                          pe_median, pe_max, pe_max_date, pe_std]
+                pe_general_result.append(pe_tmp)
+        df_pb_columns_name = ['code', 'name', 'price', 'pre_close', 'pb', 'min', 'min_date', '5_per', '10_per', 'mean',
+                              'max', 'max_date', 'std']
+        df_pb = pd.DataFrame(data=pb_general_result, columns=df_pb_columns_name)
+        # df_pb = df_pb.set_index("code")
+
+        df_pe_columns_name = ['code', 'name', 'price', 'pre_close', 'pe', 'min', 'min_date', '5_per', '10_per', 'mean',
+                              'max', 'max_date', 'std']
+        df_pe = pd.DataFrame(data=pe_general_result, columns=df_pe_columns_name)
+        # df_pe = df_pe.set_index("code")
+
+        df_pb['pb'] = df_pb['pb'].apply(pd.to_numeric)
+        df_pe['pe'] = df_pe['pe'].apply(pd.to_numeric)
+
+        # 小数只保留两位
+        columns_2decimals = ["pb", "min", "max", "mean", "std", "5_per", "10_per"]
+        df_pb[columns_2decimals] = df_pb[columns_2decimals].round(decimals=2)
+        columns_2decimals = ["pe", "min", "max", "mean", "std", "5_per", "10_per"]
+        df_pe[columns_2decimals] = df_pe[columns_2decimals].round(decimals=2)
+
+        df_pb = df_pb.sort_values(by=['pb'])
+        df_pe = df_pe.sort_values(by=['pe'])
+
+        rows = len(df_pb)
+        self.ui.tableWidget_2.setRowCount(rows)
+        for i in range(rows):
+            item = QtWidgets.QTableWidgetItem(df_pb.iloc[i, df_pb.columns.get_loc("code")])
+            self.ui.tableWidget_2.setItem(i, 0, item)
+            item = QtWidgets.QTableWidgetItem(df_pb.iloc[i, df_pb.columns.get_loc("name")])
+            self.ui.tableWidget_2.setItem(i, 1, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("price")]))
+            self.ui.tableWidget_2.setItem(i, 2, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("pre_close")]))
+            self.ui.tableWidget_2.setItem(i, 3, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("pb")]))
+            self.ui.tableWidget_2.setItem(i, 4, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("min")]))
+            self.ui.tableWidget_2.setItem(i, 5, item)
+            item = QtWidgets.QTableWidgetItem(df_pb.iloc[i, df_pb.columns.get_loc("min_date")])
+            self.ui.tableWidget_2.setItem(i, 6, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("5_per")]))
+            self.ui.tableWidget_2.setItem(i, 7, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("10_per")]))
+            self.ui.tableWidget_2.setItem(i, 8, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("mean")]))
+            self.ui.tableWidget_2.setItem(i, 9, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("max")]))
+            self.ui.tableWidget_2.setItem(i, 10, item)
+            item = QtWidgets.QTableWidgetItem(df_pb.iloc[i, df_pb.columns.get_loc("max_date")])
+            self.ui.tableWidget_2.setItem(i, 11, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pb.iloc[i, df_pb.columns.get_loc("std")]))
+            self.ui.tableWidget_2.setItem(i, 12, item)
+
+        rows = len(df_pe)
+        self.ui.tableWidget_3.setRowCount(rows)
+        for i in range(rows):
+            item = QtWidgets.QTableWidgetItem(df_pe.iloc[i, df_pe.columns.get_loc("code")])
+            self.ui.tableWidget_3.setItem(i, 0, item)
+            item = QtWidgets.QTableWidgetItem(df_pe.iloc[i, df_pe.columns.get_loc("name")])
+            self.ui.tableWidget_3.setItem(i, 1, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("price")]))
+            self.ui.tableWidget_3.setItem(i, 2, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("pre_close")]))
+            self.ui.tableWidget_3.setItem(i, 3, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("pe")]))
+            self.ui.tableWidget_3.setItem(i, 4, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("min")]))
+            self.ui.tableWidget_3.setItem(i, 5, item)
+            item = QtWidgets.QTableWidgetItem(df_pe.iloc[i, df_pe.columns.get_loc("min_date")])
+            self.ui.tableWidget_3.setItem(i, 6, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("5_per")]))
+            self.ui.tableWidget_3.setItem(i, 7, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("10_per")]))
+            self.ui.tableWidget_3.setItem(i, 8, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("mean")]))
+            self.ui.tableWidget_3.setItem(i, 9, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("max")]))
+            self.ui.tableWidget_3.setItem(i, 10, item)
+            item = QtWidgets.QTableWidgetItem(df_pe.iloc[i, df_pe.columns.get_loc("max_date")])
+            self.ui.tableWidget_3.setItem(i, 11, item)
+            item = QtWidgets.QTableWidgetItem(str(df_pe.iloc[i, df_pe.columns.get_loc("std")]))
+            self.ui.tableWidget_3.setItem(i, 12, item)
+
+    # ==============================================================
+
     def get_quote(self, code_list):
         '''
         获取股票实时行情
@@ -182,7 +355,7 @@ class watchMainWindows(object):
             """
             ts_code = [str(x).split('.')[0] for x in code_list]
             stock_rt_price = ts.get_realtime_quotes(ts_code)
-            stock_rt_price = stock_rt_price.loc[:, ["code", "price", "name"]]
+            stock_rt_price = stock_rt_price.loc[:, ["code", "price", "pre_close", "name"]]
             return stock_rt_price
 
         check_status = self.ui.checkBox_7.checkState()
@@ -204,7 +377,7 @@ class watchMainWindows(object):
         columns_name = ["code", "name", "price", "net_value", "pb2", "ret9", "index_value9"]
         if os.path.exists(r"C:\quanttime\src\watch_time\flitter.txt"):
             flitter_data = pd.read_csv(r"C:\quanttime\src\watch_time\flitter.txt",
-                                       encoding="gbk", delim_whitespace=True,names=columns_name, header=0)
+                                       encoding="gbk", delim_whitespace=True, names=columns_name, header=0)
             return flitter_data
         else:
             return pd.DataFrame()
@@ -223,6 +396,28 @@ class watchMainWindows(object):
             return 0
 
         return "%.2f%%"%(data * 100)
+
+    # ==============================================================
+    @staticmethod
+    def tscode2jqcode(x):
+        '''
+        tushare code --> joinquant code
+        即601318.SH --> 601318.XSHG
+        :param x:
+        :return:
+        '''
+        code = str(x)
+        ret = code.split('.')
+
+        if ret[0].isnumeric():
+            if ret[0][0] == '6':
+                return ret[0] + '.XSHG'
+            else:
+                return ret[0] + '.XSHE'
+
+        else:
+            print("code is not standard,code=%r ", code)
+            return "000000"
 
 
 
