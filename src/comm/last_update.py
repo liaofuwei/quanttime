c5603b7cbaef062fdd346190c8806a2f48e6063b
liaofuwei <459193023@qq.com>
2019-05-14 17:08:48 +0800

    更改函数功能，增加写入模块名与模块最后的更新日期

--- src/comm/last_update.py
+++ src/comm/last_update.py
@@ -7,41 +7,54 @@ from datetime import datetime, timedelta
 yesterday = datetime.today().date() - timedelta(days=1)
 
 
-def is_last_update_ok(date=yesterday):
+def is_last_update_ok(module_name, date=yesterday):
     '''
     判断是否已经更新过
-    程序获取更新目录价内的last_update.txt文件，读取时间
+    程序获取运行目录内的last_update.csv文件，读取模块名和时间
+    module	date
+    A	    2019/5/13
+
     与输入的日期做比较判断是否当前已经更新
+    :param module_name:模块名
     :param date: 日期
     :return: true--已经更新，false--无更新或者当天更新失败
     '''
-    last_update = pd.read_csv("last_update.txt", delim_whitespace=True,
-                              names=["date"], index_col=["date"], parse_dates=True)
-
-    if not last_update.index.empty:
-        if date > last_update.index[0].date():
-            return False
-        else:
-            return True
+    last_update = pd.read_csv("last_update.csv", index_col=["module"], parse_dates=["date"])
+    if module_name not in last_update.index:
+        print("没有module：%s的更新记录信息" % module_name)
+        return False, ""
     else:
-        print("检查last_update.txt文档")
-        return False
+        last_update_date = last_update.loc[module_name, ["date"]].date
+        if date > last_update_date.date():
+            return False, last_update_date.date().strftime("%Y-%m-%d")
+        else:
+            return True, last_update_date.date().strftime("%Y-%m-%d")
 
 
-def update_txt_record(date=yesterday):
+def update_txt_record(module_name, date=yesterday):
     '''
-    更新运行目录下的last_update.txt文档记录的最后更新时间
+    更新运行目录下的last_update.csv文档记录的最后更新时间
     默认最后更新日期为昨天
+    :param module_name:模块名
     :param date: 日期
     :return: 无
     '''
-    index = [date]
-    record = pd.DataFrame(index=index)
-    print(record)
-    record.to_csv("last_update.txt", header=False)
+    last_update = pd.read_csv("last_update.csv", index_col=["module"])
+    if module_name not in last_update.index:
+        last_update.loc[module_name] = [date]
+    else:
+        last_update.loc[module_name, ["date"]] = date
+
+    print(last_update)
+    last_update.to_csv("last_update.csv")
 
 
 if __name__ == "__main__":
-    print(is_last_update_ok())
-    update_txt_record()
+    bret, strDate = is_last_update_ok('A')
+    print(bret)
+    print(strDate)
+    bret, strDate = is_last_update_ok('B')
+    print(bret)
+    print(strDate)
+    update_txt_record("B")
 
